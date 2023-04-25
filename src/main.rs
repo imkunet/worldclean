a94a082a958b3f0c424bbd39d161ee8d285500ef
imkunet <kunettyi@gmail.com>
2023-04-25 08:51:34 -0400

    Correction of level.dat added

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::{
 
 use anyhow::{bail, Context, Result};
 use clap::Parser;
-use log::info;
+use log::{info, warn};
 use logging::setup_logging;
 use nbt::{decode::read_gzip_compound_tag, encode::write_gzip_compound_tag, CompoundTag};
 
@@ -99,9 +99,42 @@ fn process_level_data(options: Arc<CleanOpts>) -> Result<()> {
 fn apply_level_data_transformations(data_tag: &CompoundTag) -> Result<CompoundTag> {
     let mut root = CompoundTag::new();
 
+    // sort for consistency
+    let mut v: Vec<(&String, &nbt::Tag)> = data_tag.iter().collect();
+    v.sort_by(|a, b| a.0.cmp(b.0));
+
     // look for things to change the values in here!
-    for element in data_tag.iter() {
-        root.insert(element.0, element.1.clone());
+    for element in v.iter() {
+        match element.0.as_str() {
+            "allowCommands" => root.insert_bool("allowCommands", true),
+            "generatorName" => root.insert_str("generatorName", "flat"),
+            "generatorOptions" => root.insert_str("generatorOptions", "0;"),
+            "rainTime" => root.insert_i32("rainTime", i32::MAX),
+            "thunderTime" => root.insert_i32("thunderTime", i32::MAX),
+            "raining" => root.insert_bool("raining", false),
+            "thundering" => root.insert_bool("thundering", false),
+            "GameRules" => {
+                let nbt::Tag::Compound(game_rules) = element.1 else {
+                    warn!("GameRules is not a valid CompoundTag");
+                    continue;
+                };
+
+                let mut rules = CompoundTag::new();
+
+                for sub_element in game_rules.iter() {
+                    match sub_element.0.as_str() {
+                        "doDaylightCycle" => rules.insert_str("doDaylightCycle", "false"),
+                        "doMobSpawning" => rules.insert_str("doMobSpawning", "false"),
+                        "mobGriefing" => rules.insert_str("mobGriefing", "false"),
+                        "randomTickSpeed" => rules.insert_str("randomTickSpeed", "0"),
+                        _ => rules.insert(sub_element.0, sub_element.1.clone()),
+                    }
+                }
+
+                root.insert_compound_tag("GameRules", rules);
+            }
+            _ => root.insert(element.0, element.1.clone()),
+        }
     }
 
     Ok(root)
